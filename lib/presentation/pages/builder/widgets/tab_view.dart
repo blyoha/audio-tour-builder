fde374641cf22c36496873cae7996f22291f4def
blyoha <filling_box@yahoo.com>
2023-05-25 22:18:51 +0300

    Added tabview for Tour Builder Page.

---
+++ lib/presentation/pages/builder/widgets/tab_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../../../theme/theme_constants.dart';
+
+class TabView extends StatelessWidget implements PreferredSizeWidget {
+  final List<Tab> tabs;
+
+  const TabView({Key? key, required this.tabs}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(45),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 12.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: primaryColor, width: 1.0),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: TabBar(
+          tabs: tabs,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(45.0);
+}
