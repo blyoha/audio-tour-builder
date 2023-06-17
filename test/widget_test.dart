8194c8e756d91378090311f07a74e31ff1b5b9f8
blyoha <filling_box@yahoo.com>
2023-06-17 03:53:18 +0300

    Applied minor changes.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:interview/main.dart';
+import 'package:audioTourBuilder/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const TourBuilderApp());
+    await tester.pumpWidget(const TourBuilderApp(initialRoute: '',));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
