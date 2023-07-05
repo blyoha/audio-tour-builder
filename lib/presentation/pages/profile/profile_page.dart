9fd417d32cf0bb2f54e18b92a9fe1bc0ac8e8b45
blyoha <filling_box@yahoo.com>
2023-07-05 03:45:45 +0300

    Implemented Profile page.

--- lib/presentation/pages/profile/profile_page.dart
+++ lib/presentation/pages/profile/profile_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
 
 import '../../../blocs/auth/auth_bloc.dart';
-import '../../../theme/theme.dart';
+import '../../../theme/theme_constants.dart';
+import '../favorites/view/favorites_page.dart';
 
 class ProfilePage extends StatefulWidget {
   static const String route = 'profile';
@@ -14,13 +16,13 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  late AuthBloc bloc;
+  late AuthBloc authBloc;
 
   @override
   void initState() {
     super.initState();
 
-    bloc = context.read();
+    authBloc = context.read<AuthBloc>();
   }
 
   @override
@@ -29,32 +31,44 @@ class _ProfilePageState extends State<ProfilePage> {
       appBar: AppBar(
         title: const Text('Profile'),
       ),
-      body: BlocConsumer(
-        bloc: bloc,
-        listener: (context, state) {
-          if (state is AuthError) {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(const SnackBar(content: Text('Log Out failed')));
-          }
-          if (state is AuthUnauthenticated) {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(const SnackBar(content: Text('Logged Out')));
-          }
-        },
-        builder: (context, state) {
-          return Center(
-            child: MaterialButton(
-              color: AppColors.primary,
-              child: const Text('Log Out'),
-              onPressed: () {
-                if (state is AuthAuthenticated) {
-                  bloc.add(AuthSignOut());
-                }
-              },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Column(
+          children: [
+            ListTile(
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const FavoritesPage(),
+                  )),
+              title: const Text('Favorite tours'),
+              trailing: const Icon(
+                Icons.arrow_forward_ios_rounded,
+                color: Colors.black,
+              ),
             ),
-          );
-        },
+            const Gap(28.0),
+            TextButton(
+              onPressed: () async => await _logOut(),
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(errorColor),
+                foregroundColor: MaterialStatePropertyAll<Color>(Colors.white),
+              ),
+              child: const Text('Logout'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _logOut() async {
+    await authBloc.authRepo
+        .signOut()
+        .then((value) => Navigator.pushNamedAndRemoveUntil(
+              context,
+              'welcome',
+              (Route<dynamic> route) => false,
+            ));
   }
 }
