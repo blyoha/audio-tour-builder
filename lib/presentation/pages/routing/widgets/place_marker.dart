35ec615f82dc6536cc5a27e2c0737218883579be
blyoha <filling_box@yahoo.com>
2023-07-10 19:53:18 +0300

    Now map markers can be tapped to see the place info.

--- lib/presentation/pages/routing/widgets/place_marker.dart
+++ lib/presentation/pages/routing/widgets/place_marker.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../blocs/routing/routing_bloc.dart';
 import '../../../../repositories/models/place.dart';
 import '../../../../theme/theme_constants.dart';
+import 'place_info_sheet.dart';
 
 class PlaceMarker extends StatelessWidget {
   final Place place;
@@ -21,18 +22,34 @@ class PlaceMarker extends StatelessWidget {
         final color = currentPlaceId == place.key ? primaryColor : Colors.red;
         final scale = currentPlaceId == place.key ? 1.5 : 1.0;
 
-        return Transform.scale(
-          scale: scale,
-          child: Icon(
-            Icons.circle_rounded,
-            shadows: const [
-              Shadow(
-                color: Colors.black54,
-                blurRadius: 4,
-                offset: Offset(0, 1),
-              )
-            ],
-            color: color,
+        return GestureDetector(
+          onTap: () => showModalBottomSheet(
+            context: context,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+              ),
+            ),
+            builder: (context) => PlaceInfoSheet(
+              title: place.title!,
+              description: place.description!,
+              images: place.images!,
+            ),
+          ),
+          child: Transform.scale(
+            scale: scale,
+            child: Icon(
+              Icons.circle_rounded,
+              shadows: const [
+                Shadow(
+                  color: Colors.black54,
+                  blurRadius: 4,
+                  offset: Offset(0, 1),
+                )
+              ],
+              color: color,
+            ),
           ),
         );
       },
