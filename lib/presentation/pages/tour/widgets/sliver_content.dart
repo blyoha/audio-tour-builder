56989d2ce2c6a31661a4bd49627556302fd7d908
blyoha <filling_box@yahoo.com>
2023-07-10 19:54:22 +0300

    Removed excess widget. Fixed gaps and imports.

--- lib/presentation/pages/tour/widgets/sliver_content.dart
+++ lib/presentation/pages/tour/widgets/sliver_content.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 
-import '../../../../repositories/models/place.dart';
 import '../../../../repositories/models/tour.dart';
 import '../../../../repositories/tours_repository.dart';
 import '../../../../theme/theme_constants.dart';
+import 'places_list.dart';
 
 class SliverContent extends StatefulWidget {
   final Tour tour;
@@ -61,13 +61,13 @@ class _SliverContentState extends State<SliverContent> {
         widget.tour.description,
         style: const TextStyle(letterSpacing: 1.2),
       ),
-      const Gap(12.0),
+      const Gap(22.0),
       const Divider(height: 1.0, color: secondaryTextColor),
       const Gap(12.0),
       _buildParameters(10, 1.2, [1, 1, 1]),
       const Gap(12.0),
       const Divider(height: 1.0, color: secondaryTextColor),
-      const Gap(12.0),
+      const Gap(22.0),
       Text(
         "Tour route",
         style: Theme.of(context).textTheme.titleMedium,
@@ -107,32 +107,6 @@ class _SliverContentState extends State<SliverContent> {
           ],
         ),
       ],
-    );
-  }
-}
-
-class PlacesList extends StatelessWidget {
-  final List<Place> places;
-
-  const PlacesList({Key? key, required this.places}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        color: backgroundColor,
-      ),
-      child: ListView.separated(
-        shrinkWrap: true,
-        itemCount: places.length,
-        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
-        physics: const NeverScrollableScrollPhysics(),
-        separatorBuilder: (context, index) => const Gap(8.0),
-        itemBuilder: (context, index) => Text(
-          places[index].title!,
-        ),
-      ),
     );
   }
 }
