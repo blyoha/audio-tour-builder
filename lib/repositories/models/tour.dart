2674c4544d8348af132676bbf659ed6c13cda913
blyoha <filling_box@yahoo.com>
2023-07-10 18:10:42 +0300

    Added author and isLiked fields.

--- lib/repositories/models/tour.dart
+++ lib/repositories/models/tour.dart
@@ -2,38 +2,45 @@ import 'place.dart';
 
 class Tour {
   final String? key;
+  final String author;
   final String title;
   final String description;
   final double distance;
   final double time;
   final List<Place> places;
   final String? imageUrl;
+  bool isLiked;
 
   Tour({
     this.key,
+    required this.author,
     required this.title,
     required this.description,
     required this.distance,
     required this.time,
     required this.places,
     this.imageUrl,
+    this.isLiked = false,
   });
 
   factory Tour.fromJson(Map<dynamic, dynamic> json) {
     return Tour(
       key: json['key'],
+      author: json['author'],
       title: json['title'],
       description: json['description'],
       imageUrl: json['imageUrl'],
       places: json['places'],
       time: json['time'] ?? 0.0,
       distance: json['distance'] ?? 0.0,
+      isLiked: json['isLiked'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'key': key,
+      'author': author,
       'title': title,
       'description': description,
       'imageUrl': imageUrl,
@@ -44,38 +51,43 @@ class Tour {
 
   factory Tour.empty() {
     return Tour(
-      title: "",
-      description: "",
+      author: '',
+      title: '',
+      description: '',
       imageUrl: null,
       distance: 0.0,
       time: 0.0,
       places: [],
+      isLiked: false
     );
   }
 
   Tour copyWith({
     String? key,
+    String? author,
     String? title,
     String? description,
     String? imageUrl,
     double? distance,
     double? time,
+    bool? isLiked,
     List<Place>? places,
-
   }) {
     return Tour(
       key: key ?? this.key,
+      author: author ?? this.author,
       title: title ?? this.title,
       description: description ?? this.description,
       imageUrl: imageUrl ?? this.imageUrl,
       distance: distance ?? this.distance,
       time: time ?? this.time,
+      isLiked: isLiked ?? this.isLiked,
       places: places ?? this.places,
     );
   }
 
   @override
   String toString() {
-    return "$title. ${places.length} places";
+    return '$title. ${places.length} places';
   }
 }
